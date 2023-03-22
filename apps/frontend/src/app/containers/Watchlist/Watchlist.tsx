b46ab0907aca2d8e72e9262726e1e7ffe914d8fe
Louis PELARREY <louis.pelarrey@capgemini.com>
2023-03-22 11:06:38 +0100

    frontend: watchlist rework and use of useFetch()

--- apps/frontend/src/app/containers/Watchlist/Watchlist.tsx
+++ apps/frontend/src/app/containers/Watchlist/Watchlist.tsx
@@ -1,96 +1,34 @@
-import { ShowCard } from "../Home/ShowCard/ShowCard";
-import styled from "styled-components";
-import { useCallback, useContext, useEffect, useState } from "react";
-import { UserContext, UserContextProps } from "../../context/User/UserContext";
+import useFetch from "../../hooks/useFetch";
+import { Show } from "../Home/Home";
+import { Table } from "../../components/Table/Table";
+import { StyledWatchlist } from "./Watchlist.style";
 
-interface Show {
-  id: number;
-  name: string;
-  description: string;
-  imagePath: string;
-  followedBy: User[];
-}
-
-interface User {
-  id: number;
-  username: string;
+interface UseFetchFollowed {
+  data: Show[];
+  loading: boolean;
+  error?: string;
 }
 
 export function Watchlist() {
 
-  const [shows, setShows] = useState<Show[]>([]);
-  const { accessToken } = useContext<UserContextProps>(UserContext);
-
-  const StyledHome = styled.div`
-    max-width: 100vw;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    flex-shrink: 1;
-    flex-basis: 0;
-    @media (max-width: 1000px) {
-      flex-direction: column;
-    }
-  `;
-
-  /**
-   * Fetches the shows from the database
-   */
-  const fetchShowsFromDB = useCallback(async () => {
-    const res: Response = await fetch(process.env.NX_SERVER_URL + "/api/show/followed", {
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`
-      },
-    })
-
-    const fetchedShows: Show[] = await res.json()
-    return fetchedShows
-  }, [accessToken])
-
-  /**
-   * Fetches the images for the shows
-   */
-  const fetchImagesForShows = useCallback(async (shows: Show[]) => {
-    const showsWithImagesRes: Show[] = await Promise.all(
-      shows.map(
-        (show: Show) => fetch(`http://api.tvmaze.com/singlesearch/shows?q=${show.name}`)
-          .then(res => res.json())
-          .then(res => ({ ...show, imagePath: res.image?.original }))
-      )
-    )
-    return showsWithImagesRes
-  }, [])
-
-  /**
-   * Fetches the shows from the database and then fetches the images for the shows
-   */
-  const fetchShows = useCallback(async () => {
-    if (!accessToken) return;
-    try {
-      const fetchedShows: Show[] = await fetchShowsFromDB()
-      const showsWithImagesRes: Show[] = await fetchImagesForShows(fetchedShows)
-      setShows(showsWithImagesRes)
-    } catch (error) {
-      console.error(error)
-    }
-  }, [accessToken, fetchImagesForShows, fetchShowsFromDB])
-
-  useEffect(() => {
-    fetchShows()
-  }, [accessToken, fetchShows]);
-
-  if (!accessToken) return null;
+  const { data: followedShows, loading, error }: UseFetchFollowed = useFetch("/api/show/followed")
 
   return (
-    <div>
-      <StyledHome>
-        {shows.length > 0 && shows.map(show => (
-          <ShowCard key={show.id} id={show.id} name={show.name} description={show.description} imagePath={show.imagePath} likes={show.followedBy.length} />
-        ))}
-      </StyledHome>
-    </div>
+    <StyledWatchlist>
+      <h1>Watchlist</h1>
+      {loading && <p>Loading...</p>}
+
+      {error && <p>{error}</p>}
+      {followedShows && (
+        <Table columns={["Titre", "Description"]}>
+          {followedShows.map(followedShow => (
+            <tr key={followedShow.id}>
+              <td>{followedShow.name}</td>
+              <td>{followedShow.description.substring(0, 50)}...</td>
+            </tr>
+          ))}
+        </Table>
+      )}
+    </StyledWatchlist>
   );
 }
