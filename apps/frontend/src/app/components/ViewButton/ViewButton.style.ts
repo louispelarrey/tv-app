70498304760c9b3a26dd55807c711e4fc03d0823
Louis PELARREY <louis.pelarrey@capgemini.com>
2023-03-22 11:18:39 +0100

    frontend: ViewButton added

---
+++ apps/frontend/src/app/components/ViewButton/ViewButton.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledViewButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background: none;
+  font-size: 1.5rem;
+  color: darkblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  opacity: 0.9;
+`;
