f2ca97b7a5d11c62825393a191be4ed1bdccd23f
Louis PELARREY <louis.pelarrey@capgemini.com>
2023-03-22 11:19:50 +0100

    frontend: added watchlist ViewButton

--- apps/frontend/src/app/containers/Watchlist/Watchlist.tsx
+++ apps/frontend/src/app/containers/Watchlist/Watchlist.tsx
@@ -2,6 +2,9 @@ import useFetch from "../../hooks/useFetch";
 import { Show } from "../Home/Home";
 import { Table } from "../../components/Table/Table";
 import { StyledWatchlist } from "./Watchlist.style";
+import { ViewButton } from "../../components/ViewButton/ViewButton";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface UseFetchFollowed {
   data: Show[];
@@ -12,6 +15,9 @@ interface UseFetchFollowed {
 export function Watchlist() {
 
   const { data: followedShows, loading, error }: UseFetchFollowed = useFetch("/api/show/followed")
+  const navigate = useNavigate();
+
+  const onViewClick = useCallback((id: number) => navigate(`/show/${id}`), [navigate])
 
   return (
     <StyledWatchlist>
@@ -20,11 +26,12 @@ export function Watchlist() {
 
       {error && <p>{error}</p>}
       {followedShows && (
-        <Table columns={["Titre", "Description"]}>
+        <Table columns={["Titre", "Description", ""]}>
           {followedShows.map(followedShow => (
             <tr key={followedShow.id}>
               <td>{followedShow.name}</td>
               <td>{followedShow.description.substring(0, 50)}...</td>
+              <td><ViewButton onClick={() => onViewClick(followedShow.id)} /></td>
             </tr>
           ))}
         </Table>
